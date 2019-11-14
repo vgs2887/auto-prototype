9579a500c31fcaad3034d7855025dfc4d597ef52
Abbinav <abbinav97@gmail.com>
2019-11-14 17:56:08 -0600

    added changes to confirmation page

--- src/Widgets/QuoteResults/ConfirmationPage.js
+++ src/Widgets/QuoteResults/ConfirmationPage.js
@@ -1,8 +1,34 @@
 import React from "react";
 import $ from "jquery";
+import { connect } from 'react-redux'
 import "./styleconfirmation.css";
+import QuoteResultsPage from "./QuoteResultsPage";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+
+
+
+const useStyles = {
+  root: {
+      flexGrow: 1,
+      flexDirection: 'row'
+  },
+  paper: {
+      textAlign: 'center',
+  },
+}
 
 export default class ConfirmationPage extends React.Component {
+
+
+
 
   componentDidMount() {
     $("button").click(function() {
@@ -24,8 +50,84 @@ export default class ConfirmationPage extends React.Component {
             <div className="icon-fix"></div>
           </div>
         </div>
-        <div className="pol-issued">Thank you Major Alex, Your Auto policy has been processed. A confirmation and follow up steps are sent to your registered email address.</div>
-      </div>
+        
+        <div className="pol-issued">Thank you Major Alex, Your Auto policy has been processed. A confirmation and follow up steps are sent to your registered email address.</div>        
+        <div style={useStyles.root}>
+            <Card className = "conf-box">
+      
+            <CardContent>
+                <div>
+                    <ul><Typography variant="body2" color="textSecondary" component="p">
+                    Thank you Major Alex, Your Auto policy has been processed. A confirmation and follow up steps are sent to your registered email address.
+                        </Typography>
+                    </ul>
+                    <Card className = "id-details" style={{flex: 1, flexDirection: 'row'}}>
+                    <ul className = "Policy Owner">Policy Owner</ul>
+                    <ul className = "Policy Owner">Policy Number</ul>
+                    <ul className = "Policy Owner">Policy Period</ul>
+                    <ul className = "Policy Owner">Total Premium</ul>
+                    </Card>
+
+                    
+                        <ExpansionPanel className = "vehicle-expansion-panel">
+                            <ExpansionPanelSummary
+                                  expandIcon={<ExpandMoreIcon />}
+                                  aria-controls="panel1a-content"
+                                  id="panel1a-header"
+                                >
+                                  <Typography  > <img src={require("../../assets/car.png")}  width="25px"/> Vehicles</Typography>
+                            </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                          <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                          </Typography>
+                        </ExpansionPanelDetails>  
+                        </ExpansionPanel>
+
+                        <ExpansionPanel className = "drivers-expansion-panel">
+                            <ExpansionPanelSummary
+                                  expandIcon={<ExpandMoreIcon />}
+                                  aria-controls="panel1a-content"
+                                  id="panel1a-header"
+                                >
+                                  <Typography  ><img src={require("../../assets/car-driver.png")}  width="25px"/> Drivers</Typography>
+                            </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                          <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                          </Typography>
+                        </ExpansionPanelDetails>  
+                        </ExpansionPanel>
+
+                        <ExpansionPanel className = "coverages-expansion-panel">
+                            <ExpansionPanelSummary
+                                  expandIcon={<ExpandMoreIcon />}
+                                  aria-controls="panel1a-content"
+                                  id="panel1a-header"
+                                >
+                                  <Typography  ><img src={require("../../assets/umb.jpg")}  width="25px"/> Coverages</Typography>
+                            </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                          <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                          </Typography>
+                        </ExpansionPanelDetails>  
+                        </ExpansionPanel>
+                    
+                </div>
+
+            </CardContent>
+            
+            </Card>
+        </div>
+        
+
+</div>
+
+      
     );
   }
 }
