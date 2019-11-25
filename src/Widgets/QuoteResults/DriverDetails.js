50672d4bb0a133a308730265878656baea058650
anil-Madanahalli <anil.pesce@gmail.com>
2019-11-24 23:45:43 -0600

    Driver det

--- src/Widgets/QuoteResults/DriverDetails.js
+++ src/Widgets/QuoteResults/DriverDetails.js
@@ -50,7 +50,8 @@ class DriverDetails extends React.Component {
                     <span className="drivertext"><b>Drivers</b></span>
                     </Grid>
                     <Grid item xs={4}/>
-                    <Grid item xs={4}>                    
+                    <Grid item xs={4}>
+                    {/* <button className="driveradd" onClick={this.onAddDriverClick}>Add+</button> */}
                     </Grid>
                  </Grid>
                 </div>
